11d45c66f3d8a996741ebe6cdefbef282f4429cd
hari9973 <pothurihariprasad9973@gmail.com>
2021-02-15 22:16:41 +0530

    third commit

--- src/main/java/com/hari/NTAT_TASK/NtatTaskApplication.java
+++ src/main/java/com/hari/NTAT_TASK/NtatTaskApplication.java
@@ -14,7 +14,7 @@ import com.hari.NTAT_TASK.models.Users;
 import com.hari.NTAT_TASK.models.Profile;
 
 @SpringBootApplication
-public class NtatTaskApplication implements CommandLineRunner{
+public class NtatTaskApplication{
 
 	@Autowired
 	private UserRepository userRepository;
@@ -33,56 +33,5 @@ public class NtatTaskApplication implements CommandLineRunner{
 		};
 	}
 
-	@Override
-	public void run(String... args) throws Exception {
-		Users u1 = new Users("guest","guest","123456");
-		Profile p1 = new Profile("https://st3.depositphotos.com/15648834/17930/v/600/depositphotos_179308454-stock-illustration-unknown-person-silhouette-glasses-profile.jpg","10000");
-		u1.setProfile(p1);
-		userRepository.save(u1);
-
-		Users u2 = new Users("User1","User1","123456");
-		Profile p2 = new Profile("https://st3.depositphotos.com/15648834/17930/v/600/depositphotos_179308454-stock-illustration-unknown-person-silhouette-glasses-profile.jpg","10000");
-		u1.setProfile(p2);
-		userRepository.save(u2);
-
-		Users u3 = new Users("User2","User2","123456");
-		Profile p3 = new Profile("https://st3.depositphotos.com/15648834/17930/v/600/depositphotos_179308454-stock-illustration-unknown-person-silhouette-glasses-profile.jpg","10000");
-		u1.setProfile(p3);
-		userRepository.save(u3);
-
-		Users u4 = new Users("Richard Hansons","Richard Hansons","123456");
-		Profile p4 = new Profile("https://st3.depositphotos.com/15648834/17930/v/600/depositphotos_179308454-stock-illustration-unknown-person-silhouette-glasses-profile.jpg","10000");
-		u1.setProfile(p4);
-		userRepository.save(u4);
-	}
+	
 }
-
-/*@Component
-class DemoCommandLineRunner implements CommandLineRunner{
-
-	@Autowired
-	private UserRepository userRepository;
-
-	@Override
-	public void run(String... args) throws Exception {
-		Users u1 = new Users("guest","guest","123456");
-		Profile p1 = new Profile("https://st3.depositphotos.com/15648834/17930/v/600/depositphotos_179308454-stock-illustration-unknown-person-silhouette-glasses-profile.jpg","10000");
-		u1.setProfile(p1);
-		userRepository.save(u1);
-
-		Users u2 = new Users("User1","User1","123456");
-		Profile p2 = new Profile("https://st3.depositphotos.com/15648834/17930/v/600/depositphotos_179308454-stock-illustration-unknown-person-silhouette-glasses-profile.jpg","10000");
-		u1.setProfile(p2);
-		userRepository.save(u2);
-
-		Users u3 = new Users("User2","User2","123456");
-		Profile p3 = new Profile("https://st3.depositphotos.com/15648834/17930/v/600/depositphotos_179308454-stock-illustration-unknown-person-silhouette-glasses-profile.jpg","10000");
-		u1.setProfile(p3);
-		userRepository.save(u3);
-
-		Users u4 = new Users("Richard Hansons","Richard Hansons","123456");
-		Profile p4 = new Profile("https://st3.depositphotos.com/15648834/17930/v/600/depositphotos_179308454-stock-illustration-unknown-person-silhouette-glasses-profile.jpg","10000");
-		u1.setProfile(p4);
-		userRepository.save(u4);
-	}
-}*/
--- src/main/java/com/hari/NTAT_TASK/config/SecurityConfigurer.java
+++ src/main/java/com/hari/NTAT_TASK/config/SecurityConfigurer.java
@@ -10,8 +10,6 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @EnableWebSecurity
@@ -25,7 +23,7 @@ public class SecurityConfigurer extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.userDetailsService(myUserDetailsService).passwordEncoder(passwordEncoder());
+        auth.userDetailsService(myUserDetailsService);
     }
 
     @Override
@@ -41,10 +39,5 @@ public class SecurityConfigurer extends WebSecurityConfigurerAdapter {
     @Bean
     public AuthenticationManager authenticationManagerBean() throws Exception {
         return super.authenticationManagerBean();
-    }
-
-    @Bean
-    public PasswordEncoder passwordEncoder(){
-        return new BCryptPasswordEncoder();
     }
 }
